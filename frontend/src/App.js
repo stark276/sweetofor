6ea6983810b23e601c4919a8055cde6f8e37e98e
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-03-16 23:46:38 -0700

    Container added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h1>Welcome</h1>
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+        
+        
       </main>
       <Footer />
     </div>
