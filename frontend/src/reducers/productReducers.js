7576e9e80b737eddb5269e42354c68b6498d0a5e
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-03-23 03:16:24 -0700

    product detail reducer and action

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,7 +1,11 @@
 import { 
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_FAIL
+  PRODUCT_LIST_FAIL,
+
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL
  } from '../constants/productConstants'
 
 
@@ -14,6 +18,23 @@ export const productListReducers = (state={products:[]}, action) => {
       return {loading:false, products:action.payload}
 
     case PRODUCT_LIST_FAIL:
+      return {loading:false, error:action.payload}
+
+    default:
+      return state
+
+ }
+}
+
+export const productDetailsReducer = (state={product:{reviews:[]}}, action) => {
+ switch(action.type){
+    case PRODUCT_DETAILS_REQUEST:
+      return {loading:true, ...state}
+
+    case PRODUCT_DETAILS_SUCCESS:
+      return {loading:false, product:action.payload}
+
+    case PRODUCT_DETAILS_FAIL:
       return {loading:false, error:action.payload}
 
     default:
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,27 +1,35 @@
 import React, {useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProductDetails } from '../actions/productActions'
+
 
 function ProductScreen({match}) {
+  const dispatch = useDispatch()
+  const productDetails = useSelector(state => state.productDetails)
+  const {loading, error, product} = productDetails
 
-  const [product, setProduct] = useState([])
+  
 
   useEffect(() => {
-    async function fetchProduct(){
-      const {data} = await axios.get(`/api/products/${match.params.id}`)
-      setProduct(data)
-    }
-    fetchProduct()
+    dispatch(listProductDetails(match.params.id))
     
-  }, [])
-
-
+  }, [dispatch, match])
+ 
  return (
   <div>
    <Link to="/" className="btn btn-light my-3">Go Back</Link>
-   <Row>
+   {loading ?
+      <Loader />
+      : error
+      ?<Message variant='danger'>{error}</Message>
+      :
+      (
+        <Row>
     <Col md={6}>
       <Image src={product.image} alt={product.name} fluid />
     </Col>
@@ -68,6 +76,11 @@ function ProductScreen({match}) {
      </ListGroup>
     </Col>
    </Row>
+      )
+
+
+   }
+   
   </div>
  )
 }
