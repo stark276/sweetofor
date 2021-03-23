a786897c1cc907abb0b899b145ef82315df76c59
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-03-22 22:18:17 -0700

    productReducers

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,16 @@
+const productListReducers = (state={products:[]}, action) => {
+ switch(action.type){
+    case 'PRODUCT_LIST_REQUEST':
+      return {loading:true, products:[]}
+
+    case 'PRODUCT_LIST_SUCCESS':
+      return {loading:false, products:action.payload}
+
+    case 'PRODUCT_LIST_FAIL':
+      return {loading:false, error:action.payload}
+
+    default:
+      return state
+
+ }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import moduleName from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducers } from './reducers/productReducers'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  productList:productListReducers,
+})
 
 const initialState = {}
+const middleware = [thunk]
 
-const store = createStore(reducer, initialStore)
+const store = createStore(reducer, initialState,
+  composeWithDevTools(applyMiddleware(...middleware)))
 
+
+export default store
