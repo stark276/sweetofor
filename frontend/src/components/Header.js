2a37a67e5f4896b801c57ae6a33736e5631f6ef1
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-03-23 20:34:58 -0700

    cart screen preceed to checkouy

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,7 +13,7 @@ function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto"> 
-                <LinkContainer to="/card">
+                <LinkContainer to="/cart">
                   <Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
                 </LinkContainer>  
                 <LinkContainer to="login">
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,20 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducers, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
   productList:productListReducers,
   productDetails:productDetailsReducer,
+  cart: cartReducer,
 })
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+JSON.parse(localStorage.getItem('cartItems')) : []
 
-const initialState = {}
+
+const initialState = {
+  cart:{cartItems:cartItemsFromStorage}
+}
 const middleware = [thunk]
 
 const store = createStore(reducer, initialState,
