5ba2d56c649df62bd54f9883485da3e9c5a9a2ee
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-03-28 21:17:59 -0700

    RefreshToken implemented

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,7 +4,7 @@ from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serializers import ProductSerializer, UserSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 # from rest_framework_simplejwt.views import (TokenObtainPairView)
@@ -14,9 +14,11 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
   def validate(self, attrs):
 
     data = super().validate(attrs)
-    data['username'] = self.user.username
-    data['email'] = self.user.email
+    
+    serializer = UserSerializerWithToken(self.user).data
 
+    for k, v in serializer.items():
+      data[k] = v
 
     return data
    
