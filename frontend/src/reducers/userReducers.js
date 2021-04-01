c1b4e029abf413074fb95a490534fb5e7898fde1
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-04-01 00:52:21 -0700

    update user profile

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -10,7 +10,12 @@ USER_REGISTER_FAIL,
 
 USER_DETAILS_REQUEST,
 USER_DETAILS_SUCCESS,
-USER_DETAILS_FAIL,} from '../constants/userConstans'
+USER_DETAILS_FAIL,
+
+USER_UPDATE_PROFILE_REQUEST,
+USER_UPDATE_PROFILE_SUCCESS,
+USER_UPDATE_PROFILE_FAIL,
+USER_UPDATE_PROFILE_RESET } from '../constants/userConstans'
 
 
 
@@ -64,6 +69,26 @@ export const userDetailsReducer = (state={user: {}}, action) => {
 
     case USER_DETAILS_FAIL:
       return {loading:false, error:action.payload}
+
+    default:
+      return state
+
+ }
+}
+
+export const userUpdateProfileReducer = (state={}, action) => {
+ switch(action.type){
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading:true}
+
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return {loading:false,success:true, userInfo: action.payload}
+
+    case USER_UPDATE_PROFILE_FAIL:
+      return {loading:false, error:action.payload}
+
+    case USER_UPDATE_PROFILE_RESET:
+      return {}
 
     default:
       return state
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -4,7 +4,8 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUserProfile } from '../actions/userActions'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstans'
 
 function ProfileScreen({history}) {
     const [name, setName] = useState('')
@@ -22,38 +23,50 @@ function ProfileScreen({history}) {
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo } = userLogin
 
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
+    const { success } = userUpdateProfile
+
 
 
     useEffect(() => {
         if (!userInfo) {
             history.push('/login')
         } else {
-            if (!user || !user.name) {
+            if (!user || !user.name || success) {
+              dispatch({type:USER_UPDATE_PROFILE_RESET})
               dispatch(getUserDetails('profile'))
             } else {
               setName(user.name)
               setEmail(user.email)
             }
         }
-    }, [dispatch, history, userInfo, user])
+    }, [dispatch, history, userInfo, user, success])
 
     const submitHandler = (e) => {
         e.preventDefault()
-        if(password != confirmPassword) {
-          setMessage('Passwords do not match')
-        }else{
-          console.log('srghh')
-        }
-        
-    }
 
+        if (password != confirmPassword) {
+            setMessage('Passwords do not match')
+        } else {
+            dispatch(updateUserProfile({
+                'id': user._id,
+                'name': name,
+                'email': email,
+                'password': password
+            }))
+            setMessage('')
+        }
+
+    }
+  
 
   return (
     <Row>
       <Col md={3}>
         <h2>User Profile</h2>
-            {error && <Message variant='danger'>{error}</Message>}
-            {loading && <Loader />}
+            {message && <Message variant='danger'>{message}</Message>}
+                {error && <Message variant='danger'>{error}</Message>}
+                {loading && <Loader />}
             <Form onSubmit={submitHandler}>
               <Form.Group controlId='name'>
                     <Form.Label>Name</Form.Label>
