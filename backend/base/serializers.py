f6a12151d394eb8958446b8f7a2800db66e37525
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-04-02 14:47:52 -0700

    [backend] get order by id endpoit

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -48,7 +48,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
   class Meta:
     model = OrderItem
     fields = '__all__'
-class ShippingAddresstSerializer(serializers.ModelSerializer):
+class ShippingAddressSerializer(serializers.ModelSerializer):
   class Meta:
     model = ShippingAddress
     fields = '__all__'
@@ -59,24 +59,25 @@ class OrderSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
-        model = Order
-        fields = '__all__'
+      model = Order
+      fields = '__all__'
 
     def get_orderItems(self, obj):
-        items = obj.orderitem_set.all()
-        serializer = OrderItemSerializer(items, many=True)
-        return serializer.data
+      items = obj.orderitem_set.all()
+      serializer = OrderItemSerializer(items, many=True)
+      return serializer.data
 
     def get_shippingAddress(self, obj):
-        try:
-            address = ShippingAddressSerializer(
-                obj.shippingaddress, many=False).data
-        except:
-            address = False
-        return address
+
+      try:
+          address = ShippingAddressSerializer(
+              obj.shippingaddress, many=False).data
+      except:
+          address = False
+      return address
 
     def get_user(self, obj):
-        user = obj.user
-        serializer = UserSerializer(user, many=False)
-        return serializer.data
+      user = obj.user
+      serializer = UserSerializer(user, many=False)
+      return serializer.data
 
--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -61,7 +61,23 @@ def addOrderItems(request):
       #(4) update stock
       product.countInStock -= item.qty
       product.save()
-
-
     serializer = OrderSerializer(order, many = False)
   return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrderById(request, pk):
+
+    user = request.user
+
+    try:
+        order = Order.objects.get(_id=pk)
+        if user.is_staff or order.user == user:
+            serializer = OrderSerializer(order, many=False)
+            return Response(serializer.data)
+        else:
+            Response({'detail': 'Not authorized to view this order'},
+                     status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
