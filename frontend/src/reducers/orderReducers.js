0af68505c4f6a7495da167b2dc8f031ffc5f5970
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-04-02 23:19:36 -0700

    orderpay reducer and action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -3,9 +3,15 @@ ORDER_CREATE_REQUEST,
 ORDER_CREATE_SUCCESS,
 ORDER_CREATE_FAIL,
 ORDER_CREATE_RESET,
+
 ORDER_DETAILS_REQUEST,
 ORDER_DETAILS_SUCCESS,
-ORDER_DETAILS_FAIL } 
+ORDER_DETAILS_FAIL,
+/*PAY*/
+ORDER_PAY_REQUEST,
+ORDER_PAY_SUCCESS,
+ORDER_PAY_FAIL,
+ORDER_PAY_RESET,} 
 from '../constants/orderConstants'
 import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
 
@@ -53,6 +59,33 @@ export const orderDetailsReducer = (state={loading:true, orderItems:[], shipping
         loading : false,
         error: action.payload
       }
+    
+
+  
+    default:
+      return state
+  }
+
+}
+
+export const orderPayReducer = (state= {}, action) =>  {
+  switch(action.type){
+    case ORDER_PAY_REQUEST:
+      return {
+        loading : true
+      }
+    case ORDER_PAY_SUCCESS:
+      return {
+        loading : false,
+        success: true
+      }
+    case ORDER_PAY_FAIL:
+      return {
+        loading : false,
+        error: action.payload
+      }
+    case ORDER_PAY_RESET:
+      return {}
     
 
   
