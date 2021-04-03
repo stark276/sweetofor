7164ab30162105bf43a2e66738d84f4d0eb8d518
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-04-02 20:13:58 -0700

    order details reducer and action created

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -3,7 +3,11 @@ import axios from 'axios'
 import { 
 ORDER_CREATE_REQUEST,
 ORDER_CREATE_SUCCESS,
-ORDER_CREATE_FAIL } 
+ORDER_CREATE_FAIL,
+/*details*/
+ORDER_DETAILS_REQUEST,
+ORDER_DETAILS_SUCCESS,
+ORDER_DETAILS_FAIL } 
 from '../constants/orderConstants'
 import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
 
@@ -43,6 +47,43 @@ export const createOrder = (order) => async (dispatch, getState) => {
   }catch(error) {
     dispatch({
        type: ORDER_CREATE_FAIL,
+       payload: error.response && error.response.data.detail
+       ? error.response.data.detail
+       :error.message,
+     })
+  }
+}
+
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try{
+    dispatch({
+      type: ORDER_DETAILS_REQUEST
+    })
+    const {
+      userLogin:{userInfo}
+    } = getState()
+
+    const config = {
+      headers:{
+        'Content-type':'application/json',
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+    const { data } = await axios.get(
+            `/api/orders/${id}/`,
+            config
+        )
+    dispatch({
+      type: ORDER_DETAILS_SUCCESS,
+      payload:data
+    })
+
+    localStorage.removeItem('cartItems')
+
+  }catch(error) {
+    dispatch({
+       type: ORDER_DETAILS_FAIL,
        payload: error.response && error.response.data.detail
        ? error.response.data.detail
        :error.message,
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,7 +2,10 @@ import {
 ORDER_CREATE_REQUEST,
 ORDER_CREATE_SUCCESS,
 ORDER_CREATE_FAIL,
-ORDER_CREATE_RESET } 
+ORDER_CREATE_RESET,
+ORDER_DETAILS_REQUEST,
+ORDER_DETAILS_SUCCESS,
+ORDER_DETAILS_FAIL } 
 from '../constants/orderConstants'
 import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
 
@@ -25,6 +28,32 @@ export const orderCreateReducer = (state={}, action) =>  {
       }
     case ORDER_CREATE_RESET:
       return {}
+
+  
+    default:
+      return state
+  }
+
+}
+
+export const orderDetailsReducer = (state={loading:true, orderItems:[], shippingAddress:{}}, action) =>  {
+  switch(action.type){
+    case ORDER_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading : true
+      }
+    case ORDER_DETAILS_SUCCESS:
+      return {
+        loading : false,
+        order: action.payload
+      }
+    case ORDER_DETAILS_FAIL:
+      return {
+        loading : false,
+        error: action.payload
+      }
+    
 
   
     default:
