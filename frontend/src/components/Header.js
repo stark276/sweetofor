b30df682d6e7e04023e7c59e2cb96cebd98f5808
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-08-08 14:36:09 -0700

    admin page dropdown button added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -39,6 +39,21 @@ function Header() {
 
                 )}
 
+                {userInfo && userInfo.isAdmin && (
+                  <NavDropdown title='Admin' id='adminmenu'>
+                    <LinkContainer to='/admin/userlist'>
+                      <NavDropdown.Item>Users</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to='/admin/productlist'>
+                      <NavDropdown.Item>Products</NavDropdown.Item>
+                    </LinkContainer>
+
+                    <LinkContainer to='/admin/orderlist'>
+                      <NavDropdown.Item>Orders</NavDropdown.Item>
+                    </LinkContainer>
+                  </NavDropdown>
+                )}
+
                 
                 </Nav>
         </Navbar.Collapse>
